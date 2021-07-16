1bc0e53746b3bf48116f17f0ba94ab53dc6a68d2
mattyydagz <mnjdagostino@gamil.com>
2021-07-16 09:35:54 -0400

    Inital

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1cfb0e12-7a0c-464d-a553-09285e2ae1d8';
+export const MODAL_CLOSE_SEC = 2.5;
